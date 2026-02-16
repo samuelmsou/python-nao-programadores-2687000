d5e2a7c4983351a897d6bdd28245d0ab9013f780
Samuel Mendes <samuelsouza6100@gmail.com>
2026-02-16 22:43:52 +0000

    resolucao

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,14 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
-
+print(3*"samuel")
 
 # Realize uma operação entre dados do tipo inteiro e float
+print(int(altura))
+
+print(round(altura,0))
