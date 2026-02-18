06ef5882aa5c1fe854b63a24fb28d6749c8fedfe
Samuel Mendes <samuelsouza6100@gmail.com>
2026-02-18 14:06:40 +0000

    resolucao

--- exercicios/3-estruturas-de-controle/1-estrutura-condicional.py
+++ exercicios/3-estruturas-de-controle/1-estrutura-condicional.py
@@ -1,18 +1,25 @@
 # Declare 4 variáveis do tipo numérica
-
+a = 1
+b = 11
+c=3
+d=4
 
 # Crie uma estrutura condicional para comparar dois números
-
-
 # Se a condição for verdadeira, imprima na tela uma mensagem informando que a condição foi cumprida e informando o número de maior valor
-
-
 # Se a condição não for cumprida, imprima na tela uma mensagem informando que a condição é negativa e informe o número de maior valor
+
+if c!=c or d<c:
+  print(f"c ({c}) é diferente de d ({d}) ou d é menor que c")
+elif a < b and b==11*a:
+  print(f" {a} é menor que {b} e {b} é igual a {b} vezes {a}")
+else:
+  print("Nenhuma das anteriores está correta.")
+if (c*d)>=b:
+  print(f"{d} vezes {c} é {c*d}. {c*d} é maior que {b}")
+print("FIM.")
 
 
 # Insira outras condições na estrutura condicional usando o elif
-
-
 # Incremente a estrutura condicional já existente com expressões lógicas utilizando "and" ou "or"
 
 
